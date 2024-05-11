fc745337167e6883fedb4a015d1ed010b7848e22
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-05-11 19:25:22 +0530

    Update userinfo.py

--- MahakRobot/modules/userinfo.py
+++ MahakRobot/modules/userinfo.py
@@ -323,10 +323,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url=f"https://t.me/RoY_Editx/882"
+                                "ʜᴇᴀʟᴛʜ", url=f"https://t.me/God_Ravana/40"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/RoY_Editx/881"
+                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/God_Ravana/39"
                             ),
                         ],
                         [
